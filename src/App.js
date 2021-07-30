993f36f528b281704a2797f6ffeba8a007ebf612
philliez <phillh@gmail.com>
2021-07-30 17:01:26 -0600

    richest accts

--- src/App.js
+++ src/App.js
@@ -1,67 +1,40 @@
-import React from 'react';
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-import { Grid, Segment, Header } from 'semantic-ui-react'
-import axios from 'axios'
+ import React, { useState, useEffect} from 'react'
+ import axios from 'axios'
 
-class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      columnDefs: [{
-        headerName: "Balance", field: "balance"
-      }, {
-        headerName: "Address", field: "address"
-      }, {
-        headerName: "Block", field: "block"
-      }],
-           rowData: this.getListings()
-    }
+ export default function App() {
+  const url = 'https://api.helium.wtf/v1/accounts/rich?limit=20'
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(url).then(response => setData(response.data.data))
+  }, [])
+
+  const renderTable = () => {
+    return data.map(account => {
+      return (
+        <tr>
+          <td>{account.balance}</td>
+          <td>{account.address}</td>
+            </tr>
+      )
+    })
   }
-  
-  getListings() {
-  
-    axios.get('https://api.helium.wtf/v1/accounts/rich?limit=20')
-    .then((response) => {
-      let rows = []
-              for (const data in response) {
-          
-       rows.push(data)
-        }
-   
-  return rows })
+
+  return (
+    <div>
+      <h1 id="title">richest accts</h1>
+      <table id="richest"> 
+        <thead>
+          <tr>
+            <th>balance</th>
+            <th>address</th>
+         
+          </tr>
+        </thead>
+        <tbody>{renderTable()}</tbody>
+      </table>
+    </div>
+  )
 }
 
-  componentDidMount () {
-    this.getListings();
-  }
-  render() {
-    
-    return (
-        <Grid columns={3}>
-          <Grid.Row>
-            <Grid.Column width={3}></Grid.Column>
-            <Grid.Column width={10}>
-              <br />
-              <Header>Helium$tats</Header>
-              <Segment>
-                <div 
-                  className="ag-theme-balham"
-                 
-                >
-                  <AgGridReact
-                    columnDefs={this.state.columnDefs}
-                    rowData={this.state.rowData}>
-                  </AgGridReact>
-                </div>
-              </Segment>
-            </Grid.Column>
-            <Grid.Column width={3}></Grid.Column>
-          </Grid.Row>
-        </Grid>
-    );
-  }
-}
-
-export default App;
